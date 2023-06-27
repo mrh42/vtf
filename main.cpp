37815fea496eec49eac7a04e4a53f7a78fd5cab4
mrh <mrh@mrh.org>
2023-06-27 09:53:29 -0700

    experimental code to handle % (29,31,37,41).  Not entirely happy w/ this yet, but it is faster.

--- main.cpp
+++ main.cpp
@@ -8,15 +8,21 @@
 #include <cmath>
 #include <sys/time.h>
 
-// enough flags to test test for (3,5)mod8, and 0mod(primes 3 -> 23) in one shot.
+// Test for (3,5)mod8, and (0)mod(primes 3 -> 23) in one shot. From 446,185,740 potential K-values,
+// a list of 72,990,720 are left to TF test on the GPU. List is an array of 32-bit uints, using
+// about 278MB.  Each thread takes an offset from the list, adds it to the 96-bit base-K, then
+// computes P * K * 2 + 1, which is then TF tested.
+// When the entire list has been tested, K-base += M.
 //
-const uint M = 60060 * 17 * 19 * 23;
+const uint64_t M = 4 * 3L * 5 * 7 * 11 * 13 * 17 * 19 * 23;  // 446,185,740
+const uint64_t M2 = 29 * 31 * 37 * 41 * 43;
+const uint32_t ListLen = 72990720;
 
-// total threads to start, check shader, need atleast enough to to initialize the
-// DEVICE_LOCAL arrays below.
 //
-//const int np = 3317760;
-const int np = 1024*1024*48;
+// total threads to start.  choosen so each call to the gpu is around 50 to 100ms.
+//
+const int np = 1024*1024*64;
+//const int np = ListLen;
 
 // This is allocated in HOST_VISIBLE_LOCAL memory, and is shared with host.
 // it is somewhat slow, compared to DEVICE_LOCAL memory.
@@ -26,21 +32,23 @@ struct Stuff {
 	uint    Found[3];
 	uint    Debug[2];
 	uint    Init;
-	uint    L;
-	uint    Ll;
-        uint    List[M/3];
+	uint    L;            // start with 0, each thread will increment with AtomicAdd(L, 1)  
+	uint    Ll;           // ListLen, when L >= Ll, threads will return.
+        uint    List[ListLen];
+	uint    KmodM2;
+	uint    X2[M2];
 
 };
 // This is allocated in DEVICE_LOCAL memory, and is not shared with host.
 // This is much to access faster from the shader, especially if the GPU is in a PCIx1 slot.
 struct Stuff2 {
-	uint       Listx[M/3];  // copy of List.  Just a tiny speed up.
+	uint       Listx[ListLen];  // copy of List.  Just a tiny speed up.
+	uint    X2x[M2];
 };
 
-//typedef __uint128_t uint128_t;
 
 uint64_t K1, K2;
-uint64_t P;
+uint32_t P;
 
 uint DevN;
 
@@ -126,8 +134,8 @@ private:
 	VkBuffer buffer, buffer2;
 	VkDeviceMemory bufferMemory, bufferMemory2;
         
-    uint32_t bufferSize; // size of `buffer` in bytes.
-    uint32_t bufferSize2; // size of `buffer` in bytes.
+    uint64_t bufferSize; // size of `buffer` in bytes.
+    uint64_t bufferSize2; // size of `buffer` in bytes.
 
     std::vector<const char *> enabledLayers;
 
@@ -178,11 +186,14 @@ public:
 	printf("M * PmodM * 2 = %lx\n", t);
 	uint64_t k64 = K1;
 	while (1) {
-
+		
 		struct timeval t1, t2;
 		gettimeofday(&t1, NULL);
 
+		//printf("K mod M2: %ld\n", k64 % M2);
+		p->KmodM2 = uint(k64 % M2);
 		runCommandBuffer();
+		p->Init++;
 
 		gettimeofday(&t2, NULL);
 		double elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
@@ -209,9 +220,10 @@ public:
 			p->K[2] = 0;
 
 			p->L = 0;
+			p->Debug[0] = p->Debug[1] = 0;
+			p->Init = 1;
 		}
 						       
-		//p->Debug[0] = p->Debug[1] = 0;
 
 		if (k64 > K2) {
 			mrhDone = 1;
@@ -245,6 +257,15 @@ public:
 		p->Debug[0] = p->Debug[1] = 0;
 		p->Init = 0;
 
+		for (uint64_t i = 0; i < M2; i++) {
+			uint64_t q = uint64_t(P % M2) * 2 * i + 1;
+			if ((q % 29) == 0 ||(q % 31) == 0 ||(q % 37) == 0||(q % 41) == 0 || (q % 43) == 0) {
+				p->X2[i] = 0;
+			} else {
+				p->X2[i] = 1;
+			}
+		}
+
 		//
 		// This takes a second or so on the cpu, but is so much easier with 64bit ints.
 		//
@@ -257,11 +278,16 @@ public:
                         } else {
 				p->List[ones] = i;
 				ones++;
+				if (ones > ListLen) {
+					printf("Error: list longer than expected: %d.  %d isn't prime??\n", ones, P);
+					exit(1);
+				}
                         }
 		}
 		p->L = 0;
 		p->Ll = ones;
 		printf("init: %d - %0.3f sieved\n", ones, double(ones)/M);
+		//exit(1);
 		runCommandBuffer();  // initialize some shader stuff
 		printf("init done\n");
 
